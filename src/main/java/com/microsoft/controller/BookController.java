8bf8e4c527cc3e50bf7263efd3b0ead60c555ccc
964879015@qq.com <964879015@qq.com>
2020-01-20 15:57:48 +0800

    ssm frame finish

---
+++ src/main/java/com/microsoft/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.microsoft.controller;
+
+import com.microsoft.pojo.Books;
+import com.microsoft.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    @Qualifier("bookServiceImpl")
+    BookService bookService;
+
+    @RequestMapping("/allBook")
+    public String list(Model model) {
+        List<Books> list = bookService.queryAllBooks();
+        model.addAttribute("list", list);
+        return "allBook";
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -1,16 +1,11 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: ASUS
-  Date: 2020/1/20
-  Time: 9:06
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
     <title>$Title$</title>
   </head>
   <body>
-  $END$
+  <h3>
+    <a href="${pageContext.request.contextPath}/book/allBook">点击进入列表页</a>
+  </h3>
   </body>
 </html>
